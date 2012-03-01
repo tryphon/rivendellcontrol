59b22aca6c7eac66da568fb1b958aec3b2c455fc
Alban Peignier <alban@tryphon.eu>
2012-03-01 09:18:33 +0100

    Fixes Dashboard view spec

--- spec/views/dashboards/show.html.erb_spec.rb
+++ spec/views/dashboards/show.html.erb_spec.rb
@@ -2,13 +2,10 @@ require 'spec_helper'
 
 describe "/dashboards/show" do
   before(:each) do
-    assigns[:events] = @events = Array.new(3) { |n| Event.create! :severity => :info, :message => "dummy" }
     render 'dashboards/show'
   end
 
-  it "should display last events" do
-    response.should have_tag('table[class=events]') do
-      with_tag("td","Dummy", @events.size)
-    end
+  it "should display title" do
+    response.should have_tag('h1', "Dashboard")
   end
 end
