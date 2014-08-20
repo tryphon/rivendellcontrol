3b2bc7f3afd0d116ca42d72de4fe2f4437df4e80
Alban Peignier <alban@tryphon.eu>
2014-08-20 22:36:37 +0200

    Remove static secret token

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RivendellControl::Application.config.secret_token = 'be59cb931bcaec31e3eab798f6e33a350e22f3a702bced536f2f0101f5e97f409333e9c58174fe4ec04f209535f835177907b0f51697a6eff8db527b9195dac0'
